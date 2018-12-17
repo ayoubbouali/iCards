c411d561616b83e160448a4fc9a9b6c8071baeb4
Ayoub BOUALI <ayoub.bouali95@gmail.com>
2018-12-17 21:05:49 +0100

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,7 @@ export const environment = {
 
   firebase: {
  
-    apiKey: "AIzaSyCPCA9q6DmfuDupCqnGeFEJaNZqZOkyP5A",
-    authDomain: "panel-client-aa1fd.firebaseapp.com",
-    databaseURL: "https://panel-client-aa1fd.firebaseio.com",
-    projectId: "panel-client-aa1fd",
-    storageBucket: "panel-client-aa1fd.appspot.com",
-    messagingSenderId: "215224385695"
+    
  
   }
 
